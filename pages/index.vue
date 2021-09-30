306b7d7dbb351b5ce3d03906570826fc8b0e6e74
신승민 <sky2197kr@gmail.com>
2021-10-01 00:48:27 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>인덱스 페이지입니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/product.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>프로덕트 페이지입니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
