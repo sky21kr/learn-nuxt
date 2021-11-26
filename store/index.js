63c1f82c1680988515255a01bcff403dc1c2f0c6
신승민 <sky2197kr@gmail.com>
2021-11-27 01:55:55 +0900

    장바구니 목록 조회 기능 nuxtServerInit 사용

--- store/index.js
+++ store/index.js
@@ -1,3 +1,7 @@
+import { fetchCartItems } from "~/api"
+
+export const FETCH_CART_ITEMS = 'FETCH_CART_ITEMS'
+
 export const state = () => ({
     cartItems: [],
 })
@@ -9,5 +13,26 @@ export const mutations = {
             imageUrl: `${cartItem.imageUrl}?random=${Math.random()}`,
           }
         state.cartItems.push(newCartItem)
+    },
+    setCartItems(state, cartItems) {
+        state.cartItems = cartItems
+    }
+}
+
+export const actions = {
+    async [FETCH_CART_ITEMS]({ commit }) {
+        const { data } = await fetchCartItems();
+        commit('setCartItems', data.map(item => ({
+            ...item,
+            imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+          })))
+    },
+    async nuxtServerInit(storeContext, nuxtContext) {
+        await storeContext.dispatch(FETCH_CART_ITEMS)
+        // const { data } = await fetchCartItems();
+        // storeContext.commit('setCartItems', data.map(item => ({
+        //     ...item,
+        //     imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+        //   })))
     }
 }
