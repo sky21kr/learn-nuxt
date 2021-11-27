a9e2df6846a56f2f515f8b88cfd1e0ba02ddf58c
신승민 <sky2197kr@gmail.com>
2021-11-27 23:39:54 +0900

    API 함수 추가

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ const instance = axios.create({
     baseURL: process.env.baseURL,
 })
 
+function fetchProducts() {
+    return instance.get(`/products`)
+}
+
 function fetchProductById(id) {
     return instance.get(`/products/${id}`)
 }
@@ -25,4 +29,4 @@ function createCartItem(cartItem) {
     return instance.post('/carts', cartItem)
 }
 
-export { fetchProductById, fetchProductsByKeyword, createCartItem, fetchCartItems }
+export { fetchProducts, fetchProductById, fetchProductsByKeyword, createCartItem, fetchCartItems }
