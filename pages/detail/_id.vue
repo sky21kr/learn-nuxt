b2e35a206832da17e5584b78586286d27f2e3a1f
신승민 <sky2197kr@gmail.com>
2021-11-28 14:42:37 +0900

    og 태그 등록

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -31,13 +31,27 @@ export default {
     return {
       title: `Shopping item Detail - ${this.product.name}`,
       meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: `이 상품은 ${this.product.name}입니다.`,
+          },
+          {
+          hid: 'og:title',
+          property: 'og:title',
+          content: '상품 상세 페이지'
+        },
         {
-          hid: 'description',
-          name: 'description',
-          content: `이 상품은 ${this.product.name}입니다.`,
-        }
+          hid: 'og:description',
+          property: 'og:description',
+          content: '상품의 상세 정보를 확인해보세요'
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'http://placeimg.com/640/480/fashion'
+        },
       ],
-
     }
   },
   methods: {
